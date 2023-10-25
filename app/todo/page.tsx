8219d14632ef98f9a0c5f5812000d9307cdca81b
Developer Baboo <babooheerani@gmail.com>
2023-10-25 13:07:32 +0500

    added comments and fixed some bugs

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "@/lib/axios";
 import { useEffect, useState } from "react";
 
+// Define the Todo interface
 interface Todo {
   id: number;
   title: string;
@@ -11,18 +12,22 @@ interface Todo {
 
 const Todo: React.FC = () => {
 
+  // State variables for todos, title, and todo ID
   const [todos, setTodos] = useState<Todo[]>([]);
   const [title, setTitle] = useState('');
   const [todoid, setTodoId] = useState('');
 
+  // Fetch todos on component mount
   useEffect(() => {
     fetchTodos();
   }, []);
 
+  // Handle title input change
   const titleChange = (e) => {
     setTitle(e.target.value);
   }
 
+// Handle form submission
   const submitForm = (e) => {
     e.preventDefault();
   // Check if title is empty before making the request
@@ -54,6 +59,9 @@ const Todo: React.FC = () => {
   //   .catch((error) => {
   //     console.error('Error updating todo:', error);
     //   });
+
+
+  // Make a POST or PUT request based on the existence of todo ID  
   axios({
       method: todoid !== '' ? 'put' : 'post',
       url: url,
@@ -70,9 +78,10 @@ const Todo: React.FC = () => {
       });
 }
 
-
+// Function to edit a todo by ID
   function editTodo(id) {
-  setTodoId(id);
+    setTodoId(id);
+  // Find and set the title of the todo to edit
   todos.forEach((item) => {
     if (item.id === id) {
       setTitle(item.title);
@@ -81,6 +90,7 @@ const Todo: React.FC = () => {
   });
   }
 
+  // Function to delete a todo by ID
   function deleteTodo(id) {
     axios({
       method: 'delete',
@@ -94,9 +104,11 @@ const Todo: React.FC = () => {
         console.error('Error deleting todo:', error);
       });
   }
-
- function isDoneTodo(id, is_done) {
-  let params = { 'is_done': !is_done }; // Corrected object syntax
+// Function to toggle the 'is_done' property of a todo by ID
+  function isDoneTodo(id, is_done) {
+  // Invert the 'is_done' status
+    let params = { 'is_done': !is_done };
+  // Send a POST request to update 'is_done'
   axios({
     method: 'post',
     url: `api/todos/done/${id}`, // Corrected template literal syntax
@@ -110,7 +122,7 @@ const Todo: React.FC = () => {
     });
 }
 
-
+// Function to fetch todos from the server
   function fetchTodos() {
     axios
       .get('/api/todos')
@@ -122,27 +134,30 @@ const Todo: React.FC = () => {
         console.log(err);
       });
   }
+
+   // Log the updated todos whenever the 'todos' state changes
    useEffect(() => {
-    // This will log the updated state whenever `todos` changes
     console.log(todos);
-  }, [todos]);
+   }, [todos]);
+  // JSX code representing the Todo component
   return (
     <div className="container mt-5">
-      <h1 className="mb-4 text-center">Todo</h1>
+      <h1 className="mb-4 text-center">Todo Application</h1>
       <div className="row justify-content-center">
         <div className="col-sm-7">
           <form className='mt-3'  method="POST" onSubmit={submitForm}>
             <div className="input-group mb-3">
               <input type="text" className="form-control"
-                placeholder="Typing...............!"
+                placeholder="Add Todo List"
                 name='title'
                 value={title}
                 onChange={titleChange}/> 
-              <div className="input-group-append">
+              <div className="input-group-append ml-2">
                 <button type='submit' className="btn btn-primary">Save</button>
               </div>
             </div>
           </form>
+          
           <table className="table table-bordered">
             <thead>
               <tr>
@@ -152,6 +167,7 @@ const Todo: React.FC = () => {
                 <th scope="col">Action</th>
               </tr>
             </thead>
+            {todos.length > 0 ? (
             <tbody>
               {todos.map((item) => (
                 <tr key={item.id} className={item.is_done ? 'text-decoration-line-through': ''} >
@@ -178,7 +194,13 @@ const Todo: React.FC = () => {
                   </td>
                 </tr>
               ))}
-            </tbody>
+              </tbody>
+            ) : (
+              // Message when no todos are found
+              <div className="text-center mt-3" style={{ color: 'red', fontSize: '18px' }}>
+                No data found in the database.
+              </div>
+            )}
           </table>
         </div>
       </div>
